fe8429d19b90c72a4bf3b9ab3782c0ec0c3d5e14
Jake Yang <tsunghan614@gmail.com>
2014-11-14 11:00:34 -0800

    added tests

--- test/api/notes_test.js
+++ test/api/notes_test.js
@@ -1,4 +1,5 @@
-process.env.MONGO_URL = 'mongodb://localhost/notes_test';
+'use strict';
+//process.env.MONGO_URL = 'mongodb://localhost/notes_test';
 var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
@@ -7,58 +8,53 @@ require('../../server');
 
 var expect = chai.expect;
 
-describe('basic notes crud', function() {
-  var id;
-  it('should be able to create a note', function(done) {
-    chai.request('http://localhost:3000')
-    .post('/api/notes')
-    .send({noteBody: 'hello world'})
+describe('Wom backend tests', function() {
+  var email = 'test123@example.com';
+  it('should be unable to create an user with passwordConfrim fails', function(done) {
+    chai.request('https://immense-fjord-7475.herokuapp.com') //change this
+    .post('/api/users')
+    .send({'email': email,
+           'password': 'foobar123',
+           'passwordConfrim': 'foobar1234'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.noteBody).to.eql('hello world');
-      expect(res.body).to.have.property('_id');
-      id = res.body._id;
+      expect(res.text).to.have.property('passwords did not match');
       done();
     });
   });
 
-  it('should be able to get an index', function(done) {
-    chai.request('http://localhost:3000')
-    .get('/api/notes')
+  it('should be able to create an user, with a token sent back', function(done) {
+    chai.request('https://immense-fjord-7475.herokuapp.com') //change this
+    .post('/api/users')
+    .send({'email': email,
+           'password': 'foobar123',
+           'passwordConfrim': 'foobar123'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(Array.isArray(res.body)).to.be.true;
+      expect(res.body).to.have.property('jwt');
       done();
     });
   });
 
-  it('should be able to get a single note', function(done) {
-    chai.request('http://localhost:3000')
-    .get('/api/notes/' + id)
+  it('should be able to login with existing email and a token sent back', function(done) {
+    chai.request('https://immense-fjord-7475.herokuapp.com') //change this
+    .get('/api/users')
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.noteBody).to.eql('hello world');
+      expect(res.body).to.have.property('jwt');
       done();
     });
   });
 
-  it('should be able to update a note', function(done) {
-    chai.request('http://localhost:3000')
-    .put('/api/notes/' + id)
-    .send({noteBody: 'new note body'})
+  it('should be unable to create an existing user', function(done) {
+    chai.request('https://immense-fjord-7475.herokuapp.com') //change this
+    .post('/api/users')
+    .send({'email': email,
+           'password': 'foobar123',
+           'passwordConfrim': 'foobar123'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.noteBody).to.eql('new note body');
-      done();
-    });
-  });
-
-  it('should be able to destroy a note', function(done) {
-    chai.request('http://localhost:3000')
-    .delete('/api/notes/' + id)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body.msg).to.eql('success!');
+      expect(res.text).to.have.eql('cannot create that user');
       done();
     });
   });
