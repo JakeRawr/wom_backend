dccd573131a106fc9c925a1aaf5850630ef63e4e
Jake Yang <tsunghan614@gmail.com>
2014-11-18 14:19:12 -0800

    make sure we're in sync

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var passport = require('passport');
 
 var app = express();
 
+/*
 //heroku test ////////////////////
 var uriUtil = require('mongodb-uri');
 var mongodbUri = 'mongodb://heroku_app31608608:niir070ammh026ph0ujvcvlt0d@ds053380.mongolab.com:53380/heroku_app31608608';
@@ -31,10 +32,10 @@ app.use(function(req, res, next) {
 mongoose.connect(mongooseUri, options);
 
 /////////////////////////
-
+*/
 
 //local test
-//mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/wom_development');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/wom_development');
 
 
 app.use(bodyparser.urlencoded({ extended: true}));
