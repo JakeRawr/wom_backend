22e912388985133c853965bbaf634c0fdd7b86a6
taiga matsumoto <taiga418@gmail.com>
2014-11-18 19:22:23 -0800

    ready for pull

--- routes/categories_routes.js
+++ routes/categories_routes.js
@@ -6,9 +6,20 @@ module.exports = function(app){
 
 	app.get('/list',function(req,res){
 		Category.find({},function(err,data){
-   	 res.send({'name': data.name, 'list': data.list});
+		res.send(data);
+   	// res.send({'name': data.name, 'list': data.list});
     });
 	});
+
+	app.post('/add',function(req,res){
+		var cat = new Category();
+		cat.name = req.body.category;
+		cat.save(function(err,data){
+			res.send(data);
+		})
+
+	})
 };
+
 
 
