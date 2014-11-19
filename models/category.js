a208ba9656156acd8cca8b7095a5c1e28b87ab81
Jake Yang <tsunghan614@gmail.com>
2014-11-18 18:23:05 -0800

    category edited

--- models/category.js
+++ models/category.js
@@ -2,7 +2,18 @@
 var mongoose = require('mongoose');
 
 var categorySchema = mongoose.Schema({
-	list : String
+  name : 'String',
+	list : []
 });
+
+categorySchema.methods.addRest = function (rest) {
+  for (var i = 0; i < this.list.length; i++) {
+    if(this.list[i] == rest) {
+      return;
+    }
+  }
+
+  this.list.push(rest);
+};
 
 module.exports = mongoose.model('Category',categorySchema);
