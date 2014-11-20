655efed202e7866fcf22d9be95b90861129bf41a
taiga matsumoto <taiga418@gmail.com>
2014-11-20 14:00:43 -0800

    added tests

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use(function(req, res, next) {
 
 
 //mongoose.connect(mongooseUri, options);
-//mongoose.connect('mongodb://localhost/wom_development');
+mongoose.connect('mongodb://localhost/wom_development');
 
 //db test
-mongoose.connect(process.env.MONGO_URL || mongooseUri, options);
+//mongoose.connect(process.env.MONGO_URL || mongooseUri, options);
 
 
 app.use(bodyparser.urlencoded({ extended: true}));
