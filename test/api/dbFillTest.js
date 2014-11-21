162b80d4a067ed963c28d42af2bc56d4885c15d5
taiga matsumoto <taiga418@gmail.com>
2014-11-20 18:26:46 -0800

    fixed validator

--- test/api/dbFillTest.js
+++ test/api/dbFillTest.js
@@ -28,17 +28,17 @@ if(test) {
 var jwt;
 
 describe('user create/login database tests', function() {
-  var email = 'test123@example.com';
+  var email = 'test1234@example.com';
   it('should be able to create an user, with a token sent back', function (done) {
     chai.request(url) //change this
     .post('/api/users')
-    .send({'email': 'JakeYang@hotmail.com',
-           'name' : 'Jacob',
+    .send({'email': 'JackVang@gmail.com',
+           'name' : 'Jackk',
            'password': 'foobar123',
            'passwordConfirm': 'foobar123'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body).to.have.property('jwt');
+      //expect(res.body).to.have.property('jwt');
       jwt = res.body.jwt;
       done();
     });
@@ -47,8 +47,10 @@ describe('user create/login database tests', function() {
 });
 
 describe('wom database tests', function(){
-  var testaurant = 'testaurant';
-  var genre = 'burger';
+  var testaurant1 = 'pho bac';
+  var gen1 = 'pho';
+  var testaurant2 = 'pho vien anh'
+  var gen2 = 'pho';
   /*
   it('should be able to add a restaurant if the restaurant doesn\'s exist', function (done) {
     chai.request(url) //change this
@@ -65,10 +67,10 @@ describe('wom database tests', function(){
   it('should be able to add a restaurant if the restaurant doesn\'s exist', function (done) {
     chai.request(url) //change this
     .post('/rest/addRest')
-    .send({'restaurant': 'blue-moon-burgers'})
+    .send({'restaurant': testaurant})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.text).to.be.eql('blue-moon-burgers has been added');
+      expect(res.text).to.be.eql(testaurant1+ 'has been added');
       done();
     });
   });
@@ -79,7 +81,7 @@ describe('wom database tests', function(){
     .send({'restaurant': 'red-mill-burgers'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.text).to.be.eql('red-mill-burgers has been added');
+      expect(res.text).to.be.eql(testaurant2+ ' has been added');
       done();
     });
   });
@@ -89,10 +91,10 @@ describe('wom database tests', function(){
     chai.request(url) //change this
     .post('/comment/add')
     .set('jwt', jwt)
-    .send({'restaurant': 'blue-moon-burgers',
-           'rating': [4,4,5,1,4],
-           'genre': 'burger',
-           'str': 'Too many people from amazon....'})
+    .send({'restaurant': testaurant1,
+           'rating': [1,2,3,2,2],
+           'genre': gen1,
+           'str': 'This pho is alright...I guess??'})
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.text).to.be.eql('comment added');
@@ -104,10 +106,10 @@ describe('wom database tests', function(){
     chai.request(url) //change this
     .post('/comment/add')
     .set('jwt', jwt)
-    .send({'restaurant': 'red-mill-burgers',
+    .send({'restaurant': testaurant2,
            'rating': [3,3,3,3,3],
-           'genre': 'pizza',
-           'str': 'just kidding, no pizza here'})
+           'genre': gen2,
+           'str': 'I actuall don\'t know anything about pho'})
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.text).to.be.eql('comment added');
@@ -115,90 +117,6 @@ describe('wom database tests', function(){
     });
   });
 
-  it('should add a comment with a different genre with a different restaurant', function (done) {
-    chai.request(url) //change this
-    .post('/comment/add')
-    .set('jwt', jwt)
-    .send({'restaurant': 'red-mill-burgers',
-           'rating': [1,2,4,2,3],
-           'genre': 'burger',
-           'str': 'I really liked the atmosphere and the speed'})
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.text).to.be.eql('comment added');
-      done();
-    });
-  });
-
-
-  it('should display a list of comments from a restaurant', function (done) {
-    chai.request(url) //change this
-    .get('/rest/comments/' + testaurant)
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('genre');
-      expect(res.body).to.have.property('ratings');
-      done();
-    });
-  });
-
-  //url: cat/list
-  it('should display all genre lists', function (done) {
-    chai.request(url) //change this
-    .get('/genre/list')
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('list');
-      done();
-    });
-  });
-
-  it('should display genre lists from a restaurant', function (done) {
-    chai.request(url) //change this
-    .get('/rest/genres/' + testaurant)
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('list');
-      done();
-    });
-  });
-
-  it('should display the restaurants given a genre', function (done) {
-    chai.request(url) //change this
-    .get('/genre/listRests/' + genre)
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('restList');
-      done();
-    });
-  });
-
-  it('should display the rating categories array', function (done) {
-    chai.request(url) //change this
-    .get('/genre/cat/' + genre)
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('cats');
-      done();
-    });
-  });
-
-
-  it('should display avg rating of a genre of a restaurant', function (done) {
-    chai.request(url) //change this
-    .get('/rest/avg/' + genre + '/' + testaurant)
-    .set('jwt', jwt)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.have.property('avgObject');
-      done();
-    });
-  });
 
   describe('yelp api tests', function(){
     it('should be able to get a list of nearby restaurant', function (done) {
